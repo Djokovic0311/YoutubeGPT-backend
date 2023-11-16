758b647d179da12cc8084b577c637fcc95c2dee8
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2023-11-15 23:23:31 -0500

    update

--- app.py
+++ app.py
@@ -21,22 +21,4 @@ def get_transcript():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='', port=8000)
-# netlify/functions/get_transcript.py
-
-from youtube_transcript_api import YouTubeTranscriptApi
-
-def handler(event, context):
-    video_id = event['queryStringParameters']['video_id']
-    try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        transcript_texts = [item['text'] for item in transcript_list]
-        return {
-            'statusCode': 200,
-            'body': transcript_texts
-        }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': str(e)
-        }
+    app.run()
